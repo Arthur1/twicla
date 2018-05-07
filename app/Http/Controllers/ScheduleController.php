254141e85f095b92ed20e46fd1e712cb594c6875
Kazuki Asakura <32762324+arthur-teleneco@users.noreply.github.com>
2018-05-08 04:37:17 +0900

    仮完成、デバッグ状態解除

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -15,8 +15,8 @@ class ScheduleController extends Controller
 		if (empty($record['ics_url'])) return \Response::json([], 400);
 		$ical = new ICal();
 		$ical->initUrl($record['ics_url']);
-		$schedule = $ical->eventsFromRange('2018-05-01 00:00:00', '2018-08-31 00:00:00');
-		//$schedule = $ical->eventsFromInterval('1 week');
+		//$schedule = $ical->eventsFromRange('2000-01-01 00:00:00', '2099-12-31 00:00:00');
+		$schedule = $ical->eventsFromInterval('6 month');
 		return \Response::json($schedule);
 	}
 
@@ -27,8 +27,8 @@ class ScheduleController extends Controller
 		if (empty($record['ics_url'])) return \Response::json([], 400);
 		$ical = new ICal();
 		$ical->initUrl($record['ics_url']);
-		// $ts = time();
-		$ts = strtotime('8 May 2018'); // debug
+		$ts = time();
+		// $ts = strtotime('8 May 2018'); // debug
 		$schedule = $ical->eventsFromRange(date('Y-m-d', $ts).' 00:00:00', date('Y-m-d', $ts).' 23:59:00');
 		//$schedule = $ical->eventsFromInterval('1 day');
 		return \Response::json($schedule);
